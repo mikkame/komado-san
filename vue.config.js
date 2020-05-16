a8fb2b142a9d2b84969dbaa30fa7c1139656943c
mikkame <matchasoft.jp@gmail.com>
2020-05-16 20:46:55 +0900

    add config file for mac

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,18 @@ module.exports = {
     pages: {
         index: 'src/main.js',
         welcome: 'src/welcome.js'
-    }
+    },
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                mac : {
+                    extendInfo: {
+                        "NSCameraUsageDescription": "Please give us access to your camera"
+                    },
+                    "entitlements": "mac_config/entitlements.mac.plist",
+                    "entitlementsInherit": "mac_config/entitlements.mac.plist",
+                }
+            },
+        }
+    },
 }
